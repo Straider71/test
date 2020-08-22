e1b775d771a47fc109093e92de72985f5fbd2b6e
arman <arman.sabbagh@yahoo.com>
2020-08-22 17:28:56 +0430

    questionary item

---
+++ src/components/QuestionauryItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="container2">
+      <button>شروع -></button>
+      <p>وضعیت</p>
+    </div>
+    <p>تعداد</p>
+    <p>پرسشنامه</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionauryItem"
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  background-color: #eee;
+  height: 85px;
+  width: 845px;
+  display: flex;
+  justify-content: space-around;
+  /*flex-direction: ;*/
+
+  .container2 {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>

05dfb63f9768c3c95ec96938d7b021a15c433e20
arman <arman.sabbagh@yahoo.com>
2020-08-26 01:15:10 +0430

    final sign-in view

---
+++ src/components/global/CustomCheckBox.vue
@@ -0,0 +1,144 @@
+<template>
+<!--  <div class="container" :text="text">-->
+  <div class="custom-check-box-container" :text="text">
+    <input class="inp-cbx" id="morning" type="checkbox" />
+    <label class="cbx" for="morning">
+      <span>
+        <svg width="11px" height="10px">
+          <use xlink:href="#check"></use>
+        </svg>
+      </span>
+      <span>{{ text }}</span>
+    </label>
+    <svg class="inline-svg">
+      <symbol id="check" >
+        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+      </symbol>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomCheckBox",
+  props: {
+    text: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-check-box-container {
+  margin-bottom: 33px;
+  display: flex;
+  height: 20px;
+
+  .cbx {
+    position: absolute;
+    right: 0;
+
+    -webkit-user-select: none;
+    user-select: none;
+    cursor: pointer;
+    padding: 3px 4px;
+    border-radius: 6px;
+    overflow: hidden;
+    transition: all 0.2s ease;
+    &:hover {
+      /*background: rgba(0, 119, 255, 0.06);*/
+      span {
+        &:last-child {
+          border-color: #07f;
+        }
+      }
+    }
+    span {
+      float: right;
+      vertical-align: middle;
+      &:first-child {
+        position: relative;
+        width: 20px;
+        height: 20px;
+        border-radius: 5px;
+        border: 1px solid #cccfdb;
+        transition: all 0.2s ease;
+        box-shadow: 0 1px 1px rgba(0, 16, 75, 0.05);
+        svg {
+          position: absolute;
+          top: 4px;
+          left: 3px;
+          fill: none;
+          stroke: #fff;
+          stroke-width: 2;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+          stroke-dasharray: 16px;
+          stroke-dashoffset: 16px;
+          transition: all 0.3s ease;
+          transition-delay: 0.1s;
+          transform: translate3d(0, 0, 0);
+        }
+      }
+      &:last-child {
+        padding-right: 9px;
+        /*line-height: 18px;*/
+      }
+    }
+  }
+  .inp-cbx {
+    position: absolute;
+    visibility: hidden;
+    &:checked {
+      + {
+        .cbx {
+          span {
+            &:first-child {
+              background: #07f;
+              border-color: #07f;
+              animation: wave 0.4s ease;
+              svg {
+                stroke-dashoffset: 0;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .inline-svg {
+    position: absolute;
+    width: 0;
+    height: 0;
+    pointer-events: none;
+    user-select: none;
+  }
+  @media screen and (max-width: 640px) {
+    .cbx {
+      width: 100%;
+      margin-bottom: 4px;
+      display: inline-block;
+    }
+  }
+  @-moz-keyframes wave {
+    50% {
+      transform: scale(0.9);
+    }
+  }
+  @-webkit-keyframes wave {
+    50% {
+      transform: scale(0.9);
+    }
+  }
+  @-o-keyframes wave {
+    50% {
+      transform: scale(0.9);
+    }
+  }
+  @keyframes wave {
+    50% {
+      transform: scale(0.9);
+    }
+  }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="hi">
+    <div class="hi">
+  <div class="sign-in-container">
+    <p class="sub-header">چنانچه دارای حساب کاربری نیستید، لطفا ابتدا ثبت نام کنید</p>
     <form class="input-list">
       <custom-input
         class="round-top-border"
@@ -16,20 +18,39 @@
         <img src="../assets/input-img/lock.svg" />
       </custom-input>
     </form>
+    <div class="remember-forget">
+      <div class="forget-pass-link">
+        <a>رمز عبور را فراموش کرده‌اید؟</a>
+      </div>
+      <div class="remember-me">
+        <CustomCheckBox text="من را به خاطر داشته باش" />
+      </div>
+    </div>
+    <div class="buttons">
+      <CustomButton class="custom-button">
+        <p>ثبت نام</p>
+      </CustomButton>
+      <CustomButton class="custom-button">
+        <p>ورود</p>
+      </CustomButton>
+    </div>
   </div>
+    </div>
 </template>
 
 <script>
 import CustomInput from "../components/global/CustomInput";
+import CustomCheckBox from "../components/global/CustomCheckBox";
+import CustomButton from "../components/global/CustomButton";
 export default {
   name: "SignIn",
-  components: { CustomInput }
+  components: { CustomInput, CustomCheckBox, CustomButton }
 };
 </script>
 
 <style lang="scss" scoped>
 .hi {
-  background-color: #eee;
+  background-color: #f3f4f8;
   width: 100vw;
   height: 100vh;
 
@@ -38,21 +59,88 @@ export default {
   align-items: center;
 }
 
-.input-list {
-  box-shadow: 10px 8px 6px 0 #edeef5;
+.sign-in-container {
+  width: 375px;
+  max-width: 375px;
+  height: 267px;
+  display: flex;
+  flex-direction: column;
 
-  & > * {
-    margin-top: 1px;
+  & .sub-header {
+    color: #7f86aa;
+    font-size: 14px;
+    line-height: 1.36;
+    letter-spacing: -0.14px;
+    margin-bottom: 35px;
   }
 
-  & .round-top-border {
-    border-top-right-radius: 10px;
-    border-top-left-radius: 10px;
+  .input-list {
+    box-shadow: 10px 8px 6px 0 #edeef5;
+    /*margin-bottom: 33px;*/
+    margin-bottom: 33px;
+
+    & > * {
+      margin-top: 1px;
+    }
+
+    & .round-top-border {
+      border-top-right-radius: 10px;
+      border-top-left-radius: 10px;
+    }
+
+    & .round-bottom-border {
+      border-bottom-right-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
   }
 
-  & .round-bottom-border {
-    border-bottom-right-radius: 10px;
-    border-bottom-left-radius: 10px;
+  & .remember-forget {
+    width: 100%;
+    color: #7f86aa;
+    display: flex;
+    position: relative;
+
+    & .forget-pass-link {
+      position: absolute;
+      font-size: 14px;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+
+    & .remember-me {
+      font-size: 12px;
+    }
+  }
+
+  & .buttons {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+    margin: 0;
+
+    /*& .custom-button .button{*/
+    /*& .custom-button >>> button {*/
+    & .custom-button::v-deep .button {
+      height: 45px;
+      width: 183px;
+      font-size: 16px;
+      border-radius: 5px;
+    }
+
+    & .custom-button:first-child::v-deep .button {
+      background-color: #f3f4f8;
+      color: #929db3;
+      box-shadow: none;
+      border: solid 1px #d1d1d2;
+    }
+
+    & .custom-button::v-deep .button:hover {
+      background-color: #4570f5;
+      /* opacity is my opinion*/
+      opacity: 0.9;
+      color: white;
+    }
   }
 }
 </style>

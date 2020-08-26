c0fd3b7019dbf4a35e1468aeee47a04c97aff542
arman <arman.sabbagh@yahoo.com>
2020-08-26 14:36:32 +0430

    refactor files

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,52 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 // import Home from '../views/Home.vue';
-import QuestionnaireListPage from '../views/QuestionnaireListPage.vue';
-import Authenticate from '../views/Authenticate.vue';
-import SignUp from '../views/SignUp.vue';
-import SignIn from '../views/SignIn.vue';
+import QuestionnaireListPage from "../views/QuestionnaireListPage.vue";
+import Question from "../views/Question";
+import Authenticate from "../views/Authenticate.vue";
+import SignUp from "../views/SignUp.vue";
+import SignIn from "../views/SignIn.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home,
-  // },
   {
-    path: '/',
+    path: "/",
+    name: "Home",
+    component: QuestionnaireListPage
+  },
+  {
+    path: "/",
     component: Authenticate,
     children: [
       {
-        path: 'sign-in',
-        component: SignIn,
+        path: "sign-in",
+        component: SignIn
       },
       {
-        path: 'sign-up',
-        component: SignUp,
-      },
-    ],
+        path: "sign-up",
+        component: SignUp
+      }
+    ]
   },
   {
-    path: '/questionnaires',
-    name: 'QuestionnaireListPage',
-
-    component: () =>
-      import(
-        /* webpackChunkName: "about" */ '../views/QuestionnaireListPage.vue'
-      ),
-  },
+    path: "/question",
+    name: "Question",
+    component: Question
+  }
+  // {
+  //   path: '/questionnaires',
+  //   name: 'QuestionnaireListPage',
+  //
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "about" */ '../views/QuestionnaireListPage.vue'
+  //     ),
+  // },
 ];
 
 const router = new VueRouter({
-  routes,
+  routes
 });
 
 export default router;
